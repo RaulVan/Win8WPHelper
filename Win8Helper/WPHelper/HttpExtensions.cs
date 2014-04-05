1336ee221b051f58f12806ac9506fbce56031203
RaulVan <vanraul@outlook.com>
2014-04-05 16:46:08 +0800

    Add Project

---
+++ Win8Helper/WPHelper/HttpExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPHelper
+{
+    public static class HttpExtensions
+    {
+        public static Task<Stream> GetRequestStreamAsync(this HttpWebRequest request)
+        {
+            var taskComplete = new TaskCompletionSource<Stream>();
+            request.BeginGetRequestStream(ar =>
+            {
+                Stream requestStream = request.EndGetRequestStream(ar);
+                taskComplete.TrySetResult(requestStream);
+            }, request);
+            return taskComplete.Task;
+        }
+          
+
+
+
+        public static Task<HttpWebResponse> GetResponseAsync(this HttpWebRequest request)
+        {
+            var taskComplete = new TaskCompletionSource<HttpWebResponse>();
+            request.BeginGetResponse(asyncResponse =>
+            {
+                try
+                {
+                    HttpWebRequest responseRequest = (HttpWebRequest)asyncResponse.AsyncState;
+                    HttpWebResponse someResponse = (HttpWebResponse)responseRequest.EndGetResponse(asyncResponse);
+                    taskComplete.TrySetResult(someResponse);
+                }
+                catch (WebException webExc)
+                {
+                    HttpWebResponse failedResponse = (HttpWebResponse)webExc.Response;
+                    taskComplete.TrySetResult(failedResponse);
+                }
+            }, request);
+            return taskComplete.Task;
+        }
+    }
+}
---
+++ Win8Helper/Win8Helper/StorageFolderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Search;
+
+namespace Win8Helper
+{
+    public static class StorageFolderExtensions
+    {
+
+        /// <summary> 
+        /// StorageFolder 扩展方法，判断文件是否存在 
+        /// </summary> 
+        /// <param name="folder"></param> 
+        /// <param name="fileName"></param> 
+        /// <returns></returns> 
+        internal static async Task<bool> CheckFileExisted(this StorageFolder folder, string fileName)
+        {
+            return folder != null &&
+                   (await (
+                              folder.CreateFileQueryWithOptions(
+                                  new QueryOptions
+                                  {
+                                      FolderDepth = FolderDepth.Shallow,
+                                      UserSearchFilter = "System.FileName:\"" + fileName + "\""
+                                  })).GetFilesAsync()).Count > 0
+                       ? true
+                       : false;
+        }
+
+
+    }
+}
